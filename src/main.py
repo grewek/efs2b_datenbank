12c96bb2dd60a0e42527e5469f9d9733075fa34c
Kay Grewe <Kay.Grewe@protonmail.com>
2025-05-08 19:30:38 +0200

    feat: basic tableview added

--- src/main.py
+++ src/main.py
@@ -4,7 +4,17 @@ import table_menu
 
 
 def update(table_view_window):
-    table_view_window.addstr(1, 0, "this is a test",)    
+    table = table_menu.create_table(table_menu.tabular_data, ["marke", "modell", "farbe", "motorleistung", "antriebsart", "baujahr", "mietpreis"])
+
+    table_lines = table.splitlines()
+
+    #TODO: Line needs to be refactored to be more readable...
+    alignment_center= int(float(curses.COLS) / 2.0) - int((float(len(table_lines[0])) / 2.0))
+
+    i = 0
+    for line in table_lines:
+        i += 1
+        table_view_window.addstr(i, alignment_center, line)    
 
 def render(stdscr):
     stdscr.refresh()
@@ -14,7 +24,7 @@ def main(stdscr):
     screen_height = curses.LINES
     top_window = int(float(screen_height) * 0.6)    
     bottom_window = int(float(screen_height) * 0.4)
-    stdscr.halfdelay(True)
+    stdscr.nodelay(True)
 
     table_view_window = curses.newwin(top_window, screen_width, 0, 0)
     menu_view_window = curses.newwin(bottom_window, screen_width, top_window, 0)
