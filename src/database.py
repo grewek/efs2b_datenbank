c231f7513e72d66f6c500a4e70919fdad6f2d7fb
Kay Grewe <Kay.Grewe@protonmail.com>
2025-05-19 20:11:46 +0200

    feat: add first interaction between db and application

--- src/database.py
+++ src/database.py
@@ -1,9 +1,10 @@
 import mysql.connector
 from mysql.connector import Error, errorcode
+import sql_queries
 
 def establish_connection():
     try:
-        context = mysql.connector.connect(host="localhost", password="admin", user="admin", db="fuhrpark", charset="utf8mb4")
+        context = mysql.connector.connect(host="localhost", password="admin", user="admin", db="Projektarbeit", charset="utf8mb4")
         return context
     except Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -17,6 +18,12 @@ def disconnect(context):
     context.close()
 
 def query_row(context, query_string, data):
+    cursor = context.cursor()
+
+    if data:
+        cursor.execute(query_string, data)
+    else:
+        cursor.execute(query_string)
     pass
 
 def update_row(context, update_string, data):
@@ -26,4 +33,6 @@ def delete_row(context, delete_string, data):
     pass
 
 context = establish_connection()
-disconnect()
+query_row(context, sql_queries.query_all, None)
+
+disconnect(context)
