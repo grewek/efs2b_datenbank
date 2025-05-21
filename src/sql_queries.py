7ae82291746c15aef6d7086052f45501b03104da
Kay Grewe <Kay.Grewe@protonmail.com>
2025-05-21 18:16:27 +0200

    refactor: python mysql connector shenanigans...

--- src/sql_queries.py
+++ src/sql_queries.py
@@ -1,6 +1,8 @@
 query_all = "SELECT * FROM Mietwagen"
 
-insert_car = "INSERT INTO Mietwagen (Marke, Modell, Farbe, Motorleistung, Antriebsart, Baujahr, `Mietpreis pro tag`) Values (%s, %s, %s, %s, %s, %s, %s)"
+insert_car = ("INSERT INTO Mietwagen "
+    "(Marke, Modell, Farbe, Motorleistung, Antriebsart, Baujahr, `Mietpreis pro tag`) "
+    "VALUES (%s, %s, %s, %s, %s, %s, %s)")
 query_search_mark = "SELECT * FROM Mietwagen WHERE Marke = %s"
 query_search_model = "SELECT * FROM Mietwagen WHERE Modell = %s"
 query_search_drivetype = "SELECT * FROM Mietwagen WHERE Antriebsart = %s"
