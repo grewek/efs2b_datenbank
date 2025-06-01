0d287b6de374e805722303c2d6cb4751c4b9b66a
Kay Grewe <Kay.Grewe@protonmail.com>
2025-06-01 11:34:26 +0200

    refactor: removed unused classes and functions

--- src/main.py
+++ src/main.py
@@ -15,48 +15,7 @@ def main_basic():
     while not should_quit:
         should_quit = main_menu.main_menu(db)
 
-    print("ALL YOUR DATA ARE BELONG TO US")
-
-def main_curses(stdscr):
-    #Funktioniert leider nicht unter Windows...
-    screen_width = curses.COLS
-    screen_height = curses.LINES
-    top_window = int(float(screen_height) * 0.6)    
-    bottom_window = int(float(screen_height) * 0.4)
-    stdscr.nodelay(True)
-    curses.curs_set(0)
-
-    stdscr.clear()
-
-    #START TEST
-    # NOTE: This API makes working with the curses window stuff so much simpler :D I should keep this in case i need to work with curses
-    # ever again!!
-    table_window = tui_window.TuiWindow(screen_width - 1, top_window - 1, 0, 0)
-
-    table_content = table_menu.create_table(table_menu.tabular_data, ["marke", "modell", "farbe", "motorleistung", "antriebsart", "baujahr", "mietpreis"])
-    table_window.set_content(table_content.splitlines())
-
-    menu_window = tui_window.TuiWindow(screen_width - 1, bottom_window - 1, 0, top_window)
-    menu_window.set_content(MAIN_MENU_ENTRIES)
-
-    #END TEST
-     
-    while True:
-        table_window.update()
-        table_window.render(stdscr)
-
-        menu_window.update()
-        menu_window.render(stdscr)
-
-        user_input = stdscr.getch()
-
-        if user_input == ord('j'):
-            table_window.increment_position()
-        elif user_input == ord('k'):
-            table_window.decrement_position()
-        elif user_input == ord('q'):
-            break
+    print("Bis bald!")
 
 if __name__ == "__main__":
     main_basic()
-    #wrapper(main)
