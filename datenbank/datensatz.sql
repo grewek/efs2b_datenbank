5310b35890c2898b95c0c79e200fb9a1065f08e0
Kay Grewe <Kay.Grewe@protonmail.com>
2025-06-13 19:50:40 +0200

    feat: add sql example dataset

---
+++ datenbank/datensatz.sql
@@ -0,0 +1,14 @@
+INSERT INTO Mietwagen (Mietwagennr, Marke, Modell, Farbe, Motorleistung, Antriebsart, Baujahr, `Mietpreis pro Tag`) VALUES 
+(1, 'Toyota', 'Yaris', 'Rot', 130, 'Elektro', '2022-01-01', 64.0),
+(2, 'Audi', 'A4', 'Schwarz', 220, 'Diesel', '2012-01-01', 22.5),
+(3, 'Renault', 'Clio', 'Gelb', 220, 'Elektro', '2021-01-01', 22.5),
+(4, 'VW', 'Beetle', 'Grün', 100, 'Elektro', '2021-01-01', 34.68),
+(5, 'VW', 'Passat', 'Blau', 150, 'Benzin', '2020-01-01', 49.99),
+(6, 'BMW', '320d', 'Schwarz', 190, 'Diesel', '2019-01-01', 59.99),
+(7, 'Audi', 'A4', 'Weiß', 150, 'Benzin', '2020-01-01', 69.99),
+(8, 'Nissan', 'Leaf', 'Grün', 150, 'Elektro', '2021-01-01', 69.99),
+(9, 'Mercedes', 'C-Klasse', 'Grau', 204, 'Diesel', '2020-01-01', 79.99),
+(10, 'Opel', 'Astra', 'Silber', 110, 'Benzin', '2018-01-01', 39.99),
+(11, 'Renault', 'Zoe', 'Blau', 108, 'Elektro', '2022-01-01', 59.99),
+(12, 'Ford', 'Focus', 'Rot', 125, 'Benzin', '2021-01-01', 44.99),
+(13, 'Skoda', 'Octavia', 'Schwarz', 150, 'Diesel', '2019-01-01', 54.99);
